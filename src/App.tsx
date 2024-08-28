4aeb147ee3e37d6fca52893c7170398896c2063d
DracoMeteorStorm <adracometeorstorm@gmail.com>
2024-08-28 16:36:32 -0400

    cleared test

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import "./App.css";
+import God from "./god.png";
+import { Button } from "react-bootstrap";
 
 function App(): React.JSX.Element {
     return (
@@ -9,7 +11,30 @@ function App(): React.JSX.Element {
                 by Tolu
                 <br></br>
                 Hello World
+                <h1>header text</h1>
+                <img src={God} alt="god" />
+                <ul>
+                    <li>yummers.</li>
+                    <li>yummers,</li>
+                    <li>yummers!</li>
+                    <li>yummers?</li>
+                </ul>
+                <Button
+                    onClick={() => {
+                        console.log("Hello World!");
+                    }}
+                >
+                    Log Hello World
+                </Button>
+                <div
+                    style={{
+                        width: "20px",
+                        height: "10px",
+                        backgroundColor: "red",
+                    }}
+                ></div>
             </header>
+
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload.
